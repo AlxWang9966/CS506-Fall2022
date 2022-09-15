3195cf73fad1de32452fb1763c4293a0e14c40b5
AlxWang9966 <wycalex@bu.edu>
2022-09-15 13:05:40 -0400

    new cafeteria

---
+++ labs/01-lab/education/cafeteria.py
@@ -0,0 +1,3 @@
+def draw_cafeteria():
+    print("cafeteria not found")
+    return
--- labs/01-lab/education/education.py
+++ labs/01-lab/education/education.py
@@ -1,7 +1,9 @@
 from .library import draw_library
 from .school import draw_school
+from .cafeteria import cafeteria
 
 def draw_education():
     draw_library()
     draw_school()
+    draw_cafeteria()
     return
